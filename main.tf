40bff5e809b4cc8cbd197f534827debd5a0a1b75
Richard Bright <rkbright1@gmail.com>
2021-02-28 20:31:44 -0300

    add main tf file

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+  credentials = "terraform-key.json"
+
+  project = "terraform-course-306221"
+  region  = "uscentral1"
+  zone    = "uscentral1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "gcp-terraform-state-bucket"
+    prefix = "terraform1"
+    credentials = "terraform-key.json"
+  }
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
