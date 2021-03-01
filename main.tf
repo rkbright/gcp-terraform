ca2d5ae08bd835c190d9e2bda2892203aecd72bf
Richard Bright <rkbright1@gmail.com>
2021-03-01 00:55:42 -0300

    resync

--- main.tf
+++ main.tf
@@ -1,40 +1,28 @@
-provider "google" {
-  credentials = "terraform-key.json"
+module "network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "2.0.2"
+  
+  network_name = "my-vpc-network"
+  project_id   = var.project
 
-  project = "terraform-course-306221"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = var.cidr
+      subnet_region = var.region
+    },
+  ]
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-terraform {
-  backend "gcs" {
-    bucket = "gcp-terraform-state-bucket"
-    prefix = "terraform1"
-    credentials = "terraform-key.json"
+  secondary_ranges = {
+    subnet-01 = []
   }
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name = "terraform-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
-
-resource "google_compute_address" "vm_static_ip" {
-  name = "terraform-static-ip"
+module "network_fabric-net-firewall" {
+  source                  = "terraform-google-modules/network/google//modules/fabric-net-firewall"
+  version                 = "1.1.0"
+  project_id              = var.project
+  network                 = module.network.network_name
+  internal_ranges_enabled = true
+  internal_ranges         = var.cidr
 }
