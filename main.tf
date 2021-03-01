64c6f7e17b0f2be26c7fc9a45b8a8c8d87e463c4
Richard Bright <rkbright1@gmail.com>
2021-03-01 01:39:41 -0300

    configure via modules

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "network" {
-  source       = "terraform-google-modules/network/google"
-  version      = "2.0.2"
-  
+  source  = "terraform-google-modules/network/google"
+  version = "2.1.1"
+
   network_name = "my-vpc-network"
   project_id   = var.project
 
@@ -18,11 +18,40 @@ module "network" {
   }
 }
 
+module "network_routes" {
+  source       = "terraform-google-modules/network/google//modules/routes"
+  version      = "2.1.1"
+  network_name = module.network.network_name
+  project_id   = var.project
+
+  routes = [
+    {
+      name              = "egress-internet"
+      description       = "Route through IGW to internet"
+      destination_range = "0.0.0.0/0"
+      tags              = "egress-inet"
+      next_hop_internet = "true"
+    },
+  ]
+}
+
 module "network_fabric-net-firewall" {
   source                  = "terraform-google-modules/network/google//modules/fabric-net-firewall"
   version                 = "1.1.0"
   project_id              = var.project
   network                 = module.network.network_name
   internal_ranges_enabled = true
-  internal_ranges         = var.cidr
+  internal_ranges         = ["10.0.0.0/16"]
+
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+provider "google" {
+  project     = "terraform-course-306221"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+  credentials = "terraform-key.json"
 }
